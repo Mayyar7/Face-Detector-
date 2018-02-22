ebbf12c02b14ae3fb472491b434bf4c0690170b4
Meghna P Ayyar <leomi7ayyar@gmail.com>
2018-02-22 14:05:27 +0530

    Added comments

--- face_detection.py
+++ face_detection.py
@@ -8,7 +8,6 @@ def detect_faces(data_img, detector, scale_factor=1.1):
     faces = detector.detectMultiScale(gray, scaleFactor=scale_factor, minNeighbors=5)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 3)
-
     return img
 
 path = 'Faces_dataset'
@@ -16,7 +15,7 @@ haar_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 for file1 in os.listdir(path):
     im = cv2.imread(path+"\\"+file1)
     result=detect_faces(im, haar_detector, 1.3)
-    result = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+    #Play around with the scale_factor for different images as the some faces might be closer to camera than the others
     cv2.imshow('img',result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
